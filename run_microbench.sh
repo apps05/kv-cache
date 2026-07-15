e65dbfb1c57d5cf6fc93bd4ba75f6c6daaca3a9b
Aprajita Agarwal <aprajitaagarwal@Aprajitas-MacBook-Pro.local>
2026-07-15 21:12:07 +0530

    Clean up old test scripts

--- run_microbench.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "--- UNIFORM DISTRIBUTION ---"
-for shards in 1 2 4 8 16; do
-  ./build/kv_cache_microbench --clients=50 --duration=5 --read-write-ratio=90:10 --distribution=uniform --shards=$shards | grep -E "Shards|Total|Throughput"
-done
-echo "--- ZIPFIAN DISTRIBUTION ---"
-for shards in 1 2 4 8 16; do
-  ./build/kv_cache_microbench --clients=50 --duration=5 --read-write-ratio=90:10 --distribution=zipfian --zipfian-theta=0.99 --shards=$shards | grep -E "Shards|Total|Throughput"
-done
